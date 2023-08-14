d42bf8c9534ace10e728736aaff00708e862f6f1
rdsnyder08 <rdsnyder08@gmail.com>
2023-08-14 14:57:41 -0400

    middleware complete for prompt 2

--- api/utils.js
+++ api/utils.js
@@ -1,4 +1,15 @@
 function requireUser(req, res, next) {
+  if (!req.user) {
+    res.status(401)
+    next ({
+      name: "MissingUserError",
+      message: "You are required to be successfully logged in"
+
+
+    })
+  }
+
+  next()
 
 }
 
